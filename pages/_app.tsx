b5f45b136c1f26504a4ed84a8dcf7f85cfe6e944
Supawish Kaewjing <50352586+supawish34@users.noreply.github.com>
2021-09-10 13:52:35 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// eslint-disable-next-line require-jsdoc
+function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
